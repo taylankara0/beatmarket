89784e9022be955ceb1e0fbdf8ff60bca7957af6
taylankara0 <karat0tk@gmail.com>
2026-07-06 15:45:04 +0300

    feat: install s3 sdk, setup r2 client, and create upload api route

---
+++ src/app/src/lib/r2.js
@@ -0,0 +1,11 @@
+import { S3Client } from "@aws-sdk/client-s3";
+
+// Initialize our Cloudflare R2 Client using S3-compatible endpoints
+export const r2Client = new S3Client({
+  region: "auto",
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+  },
+});
