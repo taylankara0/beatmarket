ea8a70ea08c11433c1e412800a9aeb16a0a1a4dc
taylankara0 <karat0tk@gmail.com>
2026-07-21 08:18:13 +0300

    Include Iyzico resources in server bundle

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['iyzipay'],
+
+  outputFileTracingIncludes: {
+    '/api/checkout/iyzico': [
+      './node_modules/iyzipay/lib/resources/**/*',
+    ],
+    '/api/checkout/iyzico/callback': [
+      './node_modules/iyzipay/lib/resources/**/*',
+    ],
+  },
 };
 
 export default nextConfig;
